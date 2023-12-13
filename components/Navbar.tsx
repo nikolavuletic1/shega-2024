870d8c0dadf5a275ec052b8be13abe643234bf6b
nikolavuletic1 <nikola.vuletic82@gmail.com>
2023-12-13 16:39:09 +0100

    fixed small screen nav6

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -116,15 +116,9 @@ import HamburgerButton from './HamburgerIcon';
 import ArcadeButton, { Color } from './ArcadeButoon';
 import YellowBgPattern from './YellowBgPattern';
 
-
-
-
-
-
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  // const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   const handleToggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -134,21 +128,21 @@ const Navbar = () => {
     setMenuOpen(false);
   };
 
-
-
   useEffect(() => {
-
     const checkIsMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     };
-    checkIsMobile();
-    window.addEventListener('resize', checkIsMobile);
+
+    const debouncedCheckIsMobile = debounce(checkIsMobile, 200);
+
+    checkIsMobile(); // Initial check
+
+    window.addEventListener('resize', debouncedCheckIsMobile);
 
     return () => {
-      window.removeEventListener('resize', checkIsMobile);
+      window.removeEventListener('resize', debouncedCheckIsMobile);
     };
   }, []);
-
 
   const colors: Color[] = ['green', 'red', 'orange', 'blue'];
 
@@ -163,16 +157,10 @@ const Navbar = () => {
       )}
 
       {isMobile && isMenuOpen && (
-        // <div className="fixed top-0 left-0 right-0 bottom-0 bg-yellow-20 flex flex-col z-40">
-               <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col z-40">
-             <YellowBgPattern />
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col z-40">
+          <YellowBgPattern />
           <div className="flex justify-end p-4">
-            <button
-              // className="text-3xl font-bold text-yellow-500 cursor-pointer"
-              onClick={handleToggleMenu}
-            >
-
-            </button>
+            <button onClick={handleToggleMenu}></button>
           </div>
 
           <ul className="flex flex-col items-center justify-center h-full gap-12 surfaceDuo iphoneSe">
@@ -184,7 +172,6 @@ const Navbar = () => {
                 smooth={true}
                 offset={-80}
                 duration={500}
-                // className={`text-blue-50 text-[1.6rem] py-2 hover:bg-gray-300 cursor-pointer`}
                 onClick={handleLinkClick}
               >
                 <ArcadeButton color={colors[index % colors.length]}>
@@ -219,3 +206,12 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+function debounce(func: (...args: any[]) => void, delay: number) {
+  let timeout: NodeJS.Timeout;
+  return function (this: any, ...args: any[]) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), delay);
+  };
+}
