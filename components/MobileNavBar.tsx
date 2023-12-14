a5a413f084b799299d0f62944449aeadc41c9286
nikolavuletic1 <nikola.vuletic82@gmail.com>
2023-12-14 12:36:17 +0100

    fixed small screen nav2

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -1,20 +1,10 @@
 'use client'
 // MobileNavbar.js
 import React from 'react';
-// import { Link as ScrollLink } from 'react-scroll';
-// import ArcadeButton from './ArcadeButton';
-// import { NAV_LINKS } from "@/constants";
-// import { Color } from './ArcadeButton';
-
-
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
-import { useState, useEffect } from 'react';
-import { NAV_LINKS } from "@/constants";
-// import HamburgerButton from './HamburgerIcon';
+import { Link as ScrollLink } from 'react-scroll';
 import ArcadeButton, { Color } from './ArcadeButoon';
 import YellowBgPattern from './YellowBgPattern';
-
-
+import { NAV_LINKS } from '@/constants';
 
 interface MobileNavbarProps {
   isMenuOpen: boolean;
@@ -38,10 +28,10 @@ const MobileNavbar: React.FC<MobileNavbarProps> = ({
   return (
     <>
       {isMenuOpen && (
-        <div className={`fixed top-0 left-0 right-0 bottom-0 flex flex-col z-40 ${backgroundColor}`}>
+        <div className={`fixed top-0 left-0 right-0 bottom-0 flex flex-col z-[40] ${backgroundColor}`}>
           <YellowBgPattern />
           <div className="flex justify-end p-4">
-            <button onClick={handleToggleMenu}></button>
+            <button onClick={handleToggleMenu}>Close</button>
           </div>
 
           <ul className="flex flex-col items-center justify-center h-full gap-12 surfaceDuo iphoneSe">
@@ -68,4 +58,3 @@ const MobileNavbar: React.FC<MobileNavbarProps> = ({
 };
 
 export default MobileNavbar;
-
